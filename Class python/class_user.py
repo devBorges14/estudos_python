bda69b1234117f5c5c087ee57f802ff19a649150
Gabriel Borges <140124714+devBorges14@users.noreply.github.com>
2025-03-22 09:00:48 -0300

    Create class_user.py

---
+++ Class python/class_user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self,email, password, name, age, gender):
+        self.email = email
+        self.password = password
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
